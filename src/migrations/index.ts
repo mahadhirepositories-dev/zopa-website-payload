721bbebbca82e5066b7e4198a4cd09c05052b0ca
DINESH KUMAR R <rdineshkumar88@gmail.com>
2026-08-08 20:46:49 +0530

    fix: add migration for header nav children, footer columns/social tables, drop NOT NULL on link labels

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260731_083308_initial from './20260731_083308_initial';
 import * as migration_20260808_200000_add_hero_cols_and_blocks from './20260808_200000_add_hero_cols_and_blocks';
+import * as migration_20260808_210000_fix_header_footer_globals from './20260808_210000_fix_header_footer_globals';
 
 export const migrations = [
   {
@@ -11,6 +12,11 @@ export const migrations = [
     up: migration_20260808_200000_add_hero_cols_and_blocks.up,
     down: migration_20260808_200000_add_hero_cols_and_blocks.down,
     name: '20260808_200000_add_hero_cols_and_blocks'
+  },
+  {
+    up: migration_20260808_210000_fix_header_footer_globals.up,
+    down: migration_20260808_210000_fix_header_footer_globals.down,
+    name: '20260808_210000_fix_header_footer_globals'
   },
 ];
 
