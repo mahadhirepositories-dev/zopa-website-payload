70c5071762ed325d91ee35402e70bbd1f2594b8b
DINESH KUMAR R <rdineshkumar88@gmail.com>
2026-08-08 21:17:46 +0530

    fix: add missing _uuid column to version sub-tables to resolve query error

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -3,6 +3,7 @@ import * as migration_20260808_200000_add_hero_cols_and_blocks from './20260808_
 import * as migration_20260808_210000_fix_header_footer_globals from './20260808_210000_fix_header_footer_globals';
 import * as migration_20260808_215000_fix_svc_description_type from './20260808_215000_fix_svc_description_type';
 import * as migration_20260808_220000_add_svc_v_tables from './20260808_220000_add_svc_v_tables';
+import * as migration_20260808_230000_add_uuid_to_version_tables from './20260808_230000_add_uuid_to_version_tables';
 
 export const migrations = [
   {
@@ -30,7 +31,13 @@ export const migrations = [
     down: migration_20260808_220000_add_svc_v_tables.down,
     name: '20260808_220000_add_svc_v_tables'
   },
+  {
+    up: migration_20260808_230000_add_uuid_to_version_tables.up,
+    down: migration_20260808_230000_add_uuid_to_version_tables.down,
+    name: '20260808_230000_add_uuid_to_version_tables'
+  },
 ];
+
 
 
 
